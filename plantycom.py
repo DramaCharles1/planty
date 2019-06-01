1cd9c30d2faab49b0b4326f0f63dd2047c5cad18
Richard Stuart <richard.niklas.stuart@gmail.com>
2019-06-01 23:00:13 +0200

    plantycom & planty updates

--- plantycom.py
+++ plantycom.py
@@ -1,9 +1,24 @@
 from time import sleep
+from plantyData import PlantyData
 import serial
+
+
 ser = serial.Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
 #counter = 32 # Below 32 everything in ASCII is gibberish
 ser.flush()
-ser.write("TEMP=1"+'\n')
+sleep(1)
+
+data = PlantyData(0,0,25,40)
+
+print "motor: " + data.motor
+
+	
+
+while ser.in_waiting > 0:
+	print ser.readline()
+	sleep(1)
+	
+ser.write("MOIS"+'\n')
 sleep(1)
 
 while ser.in_waiting > 0:
--- sketchbook/planty/planty.ino
+++ sketchbook/planty/planty.ino
@@ -2,7 +2,7 @@
 //#define DHT11PIN 2 //Maybe needs to change
 
 //I/O
-int motorTranPin = A0; 
+int motorTranPin = A0;
 int moisSensorTranPin = A1;
 int sensorPin = A2;
 int tempSensorPin = A3;
@@ -16,7 +16,7 @@ volatile char sen = 'o';
 String ets = "";
 boolean stringComplete = false;
 boolean serialFlag = false;
- 
+
 dht DHT;
 
 void setup()
@@ -24,8 +24,8 @@ void setup()
   Serial.begin(9600);
   pinMode(motorTranPin, OUTPUT); // A0
   pinMode(moisSensorTranPin, OUTPUT); // A1
-  pinMode(boardLed, OUTPUT); 
-  digitalWrite(motorTranPin, LOW);  // turn off the motor    
+  pinMode(boardLed, OUTPUT);
+  digitalWrite(motorTranPin, LOW);  // turn off the motor
 
   pinMode(2, INPUT_PULLUP);
   //attachInterrupt(interruptPin2, turnOn, CHANGE);
@@ -38,83 +38,81 @@ void loop()
 
   String action = "";
 
-  if(serialFlag == true && stringComplete == true)
+  if (serialFlag == true && stringComplete == true)
   {
-    //If ets = "MOTR" 
+    //If ets = "MOTR"
 
-    if(checkCommand(ets))
+    if (checkCommand(ets))
     {
 
-      action = ets.substring(0,4);
-      //ets = ets.remove(1,1);
-      
-      /*
-      if(ets.endsWith('\n'))
+      action = ets.substring(0, 4);
+
+      if (ets.endsWith("\n"))
       {
-        ets = ets.replace('\n',"");
-        
-      }*/
-      
-      if(action == "MOTR")
+        ets.remove(ets.length() - 1, 1);
+
+      }
+
+      if (action == "MOTR")
       {
 
-        if(ets.substring(5).toInt() == 1)
+        if (ets.substring(5).toInt() == 1)
         {
           digitalWrite(A0, HIGH);
-          digitalWrite(boardLed, HIGH);  
-          Serial.println(ets+",OK");    
+          digitalWrite(boardLed, HIGH);
+          Serial.println(ets + ",OK");
 
         }
-        else if(ets.substring(5).toInt() == 0)
+        else if (ets.substring(5).toInt() == 0)
         {
           digitalWrite(A0, LOW);
           digitalWrite(boardLed, LOW);
-          Serial.println(ets+",OK");      
+          Serial.println(ets + ",OK");
         }
         else
         {
-          Serial.println(ets+",ERR");
-        }    
+          Serial.println(ets + ",ERR");
+        }
 
       }
-      else if(action == "MOIS")
+      else if (action == "MOIS")
       {
         //Read mois sensor
         int moisValue = readMoistureSensor();
 
-        if(moisValue == -1)
+        if (moisValue == -1)
         {
-          Serial.println(action+",ERR");        
+          Serial.println(action + ",ERR");
         }
         else
         {
-          Serial.print(ets + moisValue + ",OK");         
+          Serial.println(ets + "=" + moisValue + ",OK");
         }
 
       }
-      else if(action == "TEMP")
+      else if (action == "TEMP")
       {
         float tempHumValue = readHumTempSensor(ets.substring(5).toInt());
 
-        if(tempHumValue == -1)
+        if (tempHumValue == -1)
         {
-          
-          Serial.println(ets+",ERR");                
+
+          Serial.println(ets + ",ERR");
         }
         else
         {
-          
-          Serial.print(ets);
+
+          Serial.print(action + "=");
           Serial.print(tempHumValue);
           Serial.println(",OK");
 
-        }     
+        }
       }
     }
     else
     {
 
-      Serial.println(ets + ",ERR"); 
+      Serial.println(ets + ",ERR");
 
     }
 
@@ -126,12 +124,12 @@ void loop()
   }
 }
 
-void serialEvent(){
+void serialEvent() {
   //statements
   while (Serial.available()) {
     // get the new byte:
     char rec = (char)Serial.read();
-    // add it to the inputString:        
+    // add it to the inputString:
     ets += rec;
     if (rec == '\n') {
       stringComplete = true;
@@ -142,17 +140,17 @@ void serialEvent(){
 
 boolean checkCommand(String in)
 {
-  String action = in.substring(0,4);
+  String action = in.substring(0, 4);
 
 
-  if(action == "MOTR" || action == "MOIS" || action == "TEMP")
+  if (action == "MOTR" || action == "MOIS" || action == "TEMP")
   {
     return true;
   }
   else
   {
     return false;
-  } 
+  }
 
 }
 
@@ -174,21 +172,15 @@ float readHumTempSensor(int action)
 
   int chk = DHT.read11(tempSensorPin);
 
-  if(action == 1) //If action=1 read temperature
+  if (action == 1) //If action=1 read temperature
   {
-    humTempValue = DHT.temperature;        
+    humTempValue = DHT.temperature;
   }
-  else if(action == 2) //if action=2 read humidity
+  else if (action == 2) //if action=2 read humidity
   {
-    humTempValue = DHT.humidity;       
+    humTempValue = DHT.humidity;
   }
 
-  return humTempValue; 
+  return humTempValue;
 
 }
-
-
-
-
-
-
