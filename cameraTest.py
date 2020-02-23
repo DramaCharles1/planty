7154b5cf42bfc4011a91502892751f0e29b3488d
Richard Stuart <richard.stuart91@gmail.com>
2020-02-23 01:14:22 +0100

    green filter fix

--- cameraTest.py
+++ cameraTest.py
@@ -1,16 +1,32 @@
-
 from datetime import datetime
 from plantyCamera import plantyCamera
 import os
 from shutil import copyfile
+from xml.dom import minidom
+
+xmlPath = "/home/pi/Repo/planty" 
+xmlFile = "cameraData2.xml"
+
+fullPath = os.path.join(xmlPath, xmlFile)
+
+mydoc = minidom.parse(fullPath)
+
+CameraDatas = mydoc.getElementsByTagName('cam_data') 
+picd = CameraDatas[0].getElementsByTagName("pic_dir")[0].firstChild.data.strip()
+picCopy = CameraDatas[0].getElementsByTagName("pic_copy_dir")[0].firstChild.data.strip()
+		
+lowerGreen = [int(CameraDatas[0].getElementsByTagName("lower_green")[0].getAttribute("l1")), int(CameraDatas[0].getElementsByTagName("lower_green")[0].getAttribute("l2")), int(CameraDatas[0].getElementsByTagName("lower_green")[0].getAttribute("l3"))]
+upperGreen = [int(CameraDatas[0].getElementsByTagName("upper_green")[0].getAttribute("u1")), int(CameraDatas[0].getElementsByTagName("upper_green")[0].getAttribute("u2")), int(CameraDatas[0].getElementsByTagName("upper_green")[0].getAttribute("u3"))]
+		
 
 datime = datetime.now().replace(microsecond=0).isoformat()
 
-picd = '/media/pi/USB'
-picCopy = '/var/www/html/Images'
-picn = str(datime) + ".jpg"
+picn = "2020-02-22T12:00:11.jpg"
+picn = "2020-02-20T12:00:11.jpg"
+picn = "2020-02-19T12:00:12.jpg"
+#picn = "/media/pi/USB/2020-02-22T12:00:11.jpg"
 
-plantCam = plantyCamera(picd, picn, picCopy)
+plantCam = plantyCamera(picd, picn, picCopy, lowerGreen, upperGreen)
 
 #plantCam.checkPicDir()
 
@@ -19,13 +35,13 @@ plantCam = plantyCamera(picd, picn, picCopy)
 # else:
 	# print "not ok"
 
-plantCam.getPic()
+#plantCam.getPic()
 
-print("ok")	
+#print("ok")	
 
-plantCam.copyPic()
+#plantCam.copyPic()
 
-print("done copy")
+#print("done copy")
 print("pic: " + plantCam.fullPath)
 print("pic copy: " + plantCam.fullCopyPath)
 
@@ -34,7 +50,3 @@ plantCam.greenCheck()
 print("Original pixel: " + str(plantCam.org_pixel))
 print("Green pixel: " + str(plantCam.green_pixel))
 print("Green pixel percentage: " + str(plantCam.green_percentage))
-
-print(str(type(plantCam.org_pixel)))
-print(str(type(plantCam.green_pixel)))
-print(str(type(plantCam.green_percentage)))
